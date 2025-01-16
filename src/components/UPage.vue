2d4de816a9a390bdbbcd357f6ea9577b2b5e9b05
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-01-16 11:53:28 +0300

    added category

--- src/components/UPage.vue
+++ src/components/UPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="flex flex-col items-center gap-4 h-screen pb-[20vh] w-full bg-smoke-100">
+    <main class="flex flex-col items-center h-screen pb-[20vh] w-full bg-smoke-100">
         <slot></slot>
     </main>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,15 +14,26 @@ import UPage from '@/components/UPage.vue';
         <UH1>Фото и видеокамеры</UH1>
         <h2 class="text-2xl text-accent">3ИСИП-722</h2>
       </div>
-      <section class="flex flex-col gap-2 w-full max-w-[1000px] px-5">
+      <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 w-full max-w-[1000px] px-5">
         <div class="flex flex-col gap-2 w-full">
           <h1 class="text-xl">Лабораторные работы</h1>
           <LinkButton href="/laboratory_work/1">
             Лабораторная работа 1
           </LinkButton>
         </div>
+        <div class="flex flex-col gap-2 w-full">
+          <h1 class="text-xl">Доработки</h1>
+          <LinkButton href="/">
+            ---
+          </LinkButton>
+        </div>
+        <div class="flex flex-col gap-2 w-full">
+          <h1 class="text-xl">Учебная практика</h1>
+          <LinkButton href="/">
+            ---
+          </LinkButton>
+        </div>
       </section>
     </UMain>
   </UPage>
-  <UHeader />
 </template>
