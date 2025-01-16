cb0630e287893a12a9b8a17180fcb8c3c90aaf1e
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-01-16 20:25:25 +0300

    Update LinkButton.vue

--- src/components/LinkButton.vue
+++ src/components/LinkButton.vue
@@ -1,6 +1,5 @@
 <template>
     <a  
-        target="_blank" 
         class="w-full bg-white border border-smoke-200 text-accent rounded-lg p-3 hover:bg-gray-100 transition-all shadow-sm">
         <slot></slot>
     </a>
