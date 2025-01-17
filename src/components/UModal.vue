9abdb005c44715aa78a649cdb837134e9efe794c
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-01-17 16:57:45 +0300

    completed practical work 1

--- src/components/UModal.vue
+++ src/components/UModal.vue
@@ -8,7 +8,7 @@ const isVisible = defineModel<boolean>();
         <div v-if="isVisible" class="fixed z-50 top-0 left-0 w-full h-screen bg-black/70 transition duration-200">
             <div class="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-white size-3/4 rounded-lg p-4">
                 <button @click="isVisible=!isVisible"
-                    class="absolute p-0 top-4 right-4 flex justify-center items-center size-8 rounded-full bg-smoke-200 hover:bg-smoke-100 transition-all duration-200">
+                    class="absolute p-0 top-4 right-4 flex justify-center items-center size-8 rounded-full border border-smoke-200 hover:bg-smoke-200 transition-all duration-200">
                     <span class="material-symbols-outlined text-accent">
                         close
                     </span>
--- src/pages/polygon.vue
+++ src/pages/polygon.vue
@@ -45,45 +45,50 @@ let openModalWithImg = (path: string) => {
     <UPage>
         <UHeader />
         <UMain>
-            <section>
-                <div class="grid grid-cols-2 gap-4 w-full p-4 bg-white border border-smoke-200 rounded-lg">
-                    <UButton @click="openModalWithImg('cart/figma.png')">
-                        <div class="flex items-center justify-between">
-                            <span>Figma</span>
-                            +
-                        </div>
-                    </UButton>
-                    <UButton @click="openModalWithImg('cart/uml_ozon.png')">
-                        <div class="flex items-center justify-between">
-                            <span>Ozon</span>
-                            +
-                        </div>
-                    </UButton>
-                    <UButton @click="openModalWithImg('cart/uml_aliexpress.png')">
-                        <div class="flex items-center justify-between">
-                            <span>Aliexpress</span>
-                            +
-                        </div>
-                    </UButton>
-                    <UButton @click="openModalWithImg('cart/uml_wb.png')">
-                        <div class="flex items-center justify-between">
-                            <span>Wildberries</span>
-                            +
-                        </div>
-                    </UButton>
-                    <UButton @click="openModalWithImg('cart/uml.png')">
-                        <div class="flex items-center justify-between">
-                            <span>UML</span>
-                            +
-                        </div>
-                    </UButton>
+            <section class="flex flex-col gap-2">
+                <UH1>Корзина</UH1>
+                <div class="flex flex-col w-full p-4 bg-white border border-smoke-200 rounded-lg gap-4">
+                    <div class="grid grid-cols-2 gap-4 ">
+                        <UButton @click="openModalWithImg('cart/figma.png')">
+                            <div class="flex items-center justify-between">
+                                <span>Figma</span>
+                                +
+                            </div>
+                        </UButton>
+                        <UButton @click="openModalWithImg('cart/uml_ozon.png')">
+                            <div class="flex items-center justify-between">
+                                <span>Ozon</span>
+                                +
+                            </div>
+                        </UButton>
+                        <UButton @click="openModalWithImg('cart/uml_aliexpress.png')">
+                            <div class="flex items-center justify-between">
+                                <span>Aliexpress</span>
+                                +
+                            </div>
+                        </UButton>
+                        <UButton @click="openModalWithImg('cart/uml_wb.png')">
+                            <div class="flex items-center justify-between">
+                                <span>Wildberries</span>
+                                +
+                            </div>
+                        </UButton>
+                        <UButton @click="openModalWithImg('cart/uml.png')">
+                            <div class="flex items-center justify-between">
+                                <span>UML</span>
+                                +
+                            </div>
+                        </UButton>
+                    </div>
+                    <a class="text-accent hover:text-black transition-all"
+                        href="https://disk.yandex.ru/i/LyUJ6qOu92cMlg">Ссылка на отчет</a>
                 </div>
             </section>
         </UMain>
     </UPage>
     <UModal v-model="showModal">
-        <div class="w-full h-full flex justify-center items-center">
-            <div class="flex justify-center items-center w-3/5 h-4/5 rounded-lg overflow-hidden relative"
+        <div class="w-full h-full flex flex-col gap-2 justify-center items-center">
+            <div class="flex justify-center items-center w-3/5 h-4/5 rounded-lg overflow-hidden relative border border-smoke-200"
                 @mousemove="onMouseMove" @mouseleave="onMouseLeave">
                 <img ref="image" class="object-contain w-full h-full transition-transform duration-700"
                     :src="imagePath" />
