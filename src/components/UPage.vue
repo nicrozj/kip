0de5d11f1414f49c6a8109b1a86c9672f070010f
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-01-24 00:33:00 +0300

    fix bag with bg

--- src/components/UPage.vue
+++ src/components/UPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="flex flex-col items-center h-screen pb-40 w-full bg-smoke-100">
+    <main class="flex flex-col items-center  min-h-screen h-full pb-40 w-full bg-smoke-100">
         <slot></slot>
     </main>
 </template>
