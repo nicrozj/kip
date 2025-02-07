d7b8e957440c00d83eed2da3663ac9f03915b215
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-07 14:55:22 +0300

    add laboratories component

---
+++ src/pages/laboratories/Laboratories.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import LinkButton from "@/components/LinkButton.vue";
+import UHeader from "@/components/UHeader.vue";
+import UMain from "@/components/UMain.vue";
+import UPage from "@/components/UPage.vue";
+import { ref } from "vue";
+</script>
+
+<template>
+  <UPage>
+    <UHeader />
+    <UMain>
+      <div class="text-center my-10">
+        <h2 class="text-2xl text-accent">Лабараторные работы</h2>
+      </div>
+      <div class="flex flex-col gap-2 w-full">
+        <section
+          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 w-full max-w-[1000px] px-5"
+        >
+          <div class="flex flex-col gap-2 w-full">
+            <LinkButton href="/laboratory1">
+              Лабораторная работа #1
+            </LinkButton>
+          </div>
+          <div class="flex flex-col gap-2 w-full">
+            <LinkButton href="/laboratory2">
+              Лабораторная работа #2
+            </LinkButton>
+          </div>
+          <div class="flex flex-col gap-2 w-full">
+            <LinkButton href="/laboratory3">
+              Лабораторная работа #3
+            </LinkButton>
+          </div>
+          <div class="flex flex-col gap-2 w-full">
+            <LinkButton href="/laboratory4">
+              Лабораторная работа #4
+            </LinkButton>
+          </div>
+          <div class="flex flex-col gap-2 w-full">
+            <LinkButton href="/laboratory5">
+              Лабораторная работа #5
+            </LinkButton>
+          </div>
+        </section>
+      </div>
+    </UMain>
+  </UPage>
+</template>
